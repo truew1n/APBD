d98240597ed45e747feca741c2866b8dbcc9cb74
truew1n <48839784+truew1n@users.noreply.github.com>
2024-03-13 22:42:30 +0100

    Branch test

--- LAB02/Test/Test/Program.cs
+++ LAB02/Test/Test/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            for(int i = 0; i < 20; ++i)
+            {
+                Console.WriteLine("i = " + i);
+            }
         }
     }
 }
