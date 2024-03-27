0d20e79ce72c47adb3963702a7a87b9d8e5a7a9a
truew1n <48839784+truew1n@users.noreply.github.com>
2024-03-27 19:54:58 +0100

    Add files via upload

---
+++ LAB03/LAB03/LiquidContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03
+{
+    public class LiquidContainer : Container, IHazardNotifier
+    {
+        public bool IsHazardous { get; private set; }
+
+        public LiquidContainer(double cargoMass, int height, double tareWeight, int depth, string serialNumber, double maxLoad, bool isHazardous)
+            : base(cargoMass, height, tareWeight, depth, serialNumber, maxLoad)
+        {
+            IsHazardous = isHazardous;
+        }
+
+        public override void LoadCargo(double mass)
+        {
+            double maxCapacity = IsHazardous ? MaxLoad * 0.5 : MaxLoad * 0.9;
+            if (mass > maxCapacity)
+            {
+                SendHazardNotification($"Attempted to overload a {(IsHazardous ? "hazardous" : "non-hazardous")} liquid container.");
+                throw new OverfillException("Cannot load beyond max capacity.");
+            }
+            CargoMass = mass;
+        }
+
+        public override void UnloadCargo()
+        {
+            CargoMass = 0;
+        }
+
+        public void SendHazardNotification(string message)
+        {
+            Console.WriteLine($"Hazard Notification for {SerialNumber}: {message}");
+        }
+    }
+}
---
+++ LAB03/LAB03/OverfillException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03
+{
+    public class OverfillException : Exception
+    {
+        public OverfillException(string message) : base(message)
+        {
+        }
+    }
+
+}
