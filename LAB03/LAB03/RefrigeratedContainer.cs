3e5c4cd41c28ed619bce32c1ccde4b91878fbb02
truew1n <48839784+truew1n@users.noreply.github.com>
2024-03-27 19:57:35 +0100

    Add files via upload

---
+++ LAB03/LAB03/RefrigeratedContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03
+{
+    public class RefrigeratedContainer : Container
+    {
+        public string ProductType { get; private set; }
+        public double Temperature { get; private set; }
+
+        public RefrigeratedContainer(double cargoMass, int height, double tareWeight, int depth, string serialNumber, double maxLoad, string productType, double temperature)
+            : base(cargoMass, height, tareWeight, depth, serialNumber, maxLoad)
+        {
+            ProductType = productType;
+            Temperature = temperature;
+        }
+
+        public override void LoadCargo(double mass)
+        {
+            if (mass > MaxLoad)
+            {
+                throw new OverfillException("Cannot load beyond max capacity.");
+            }
+            CargoMass = mass;
+        }
+
+        public override void UnloadCargo()
+        {
+            CargoMass = 0;
+        }
+    }
+}
