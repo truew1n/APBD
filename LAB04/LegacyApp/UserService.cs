e82aa211cc1c88ff51dfa03b2876a73e5a7c7418
truew1n <48839784+truew1n@users.noreply.github.com>
2024-04-02 22:53:24 +0200

    ez

---
+++ LAB04/LegacyApp/UserService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+
+        public bool IsBasicInfoNull(string firstName, string lastName)
+        {
+            return string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName);
+        }
+        public bool IsEmailValid(string email)
+        {
+            return email.Contains("@") || email.Contains(".");
+        }
+
+        public void ApplyCreditLimit(User user, Client client)
+        {
+            if (client.Type == "VeryImportantClient") user.HasCreditLimit = false;
+            else
+            {
+                bool bClientType = (client.Type == "ImportantClient");
+                user.HasCreditLimit = bClientType ? user.HasCreditLimit : true;
+                using (UserCreditService userCreditService = new UserCreditService())
+                {
+                    user.CreditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth) * (bClientType ? 2 : 1);
+                }
+            }
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (IsBasicInfoNull(firstName, lastName)) return false;
+            
+            if (!IsEmailValid(email)) return false;
+            
+            DateTime now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            
+            if (age < 21) return false;
+            
+            ClientRepository clientRepository = new ClientRepository();
+            Client client = clientRepository.GetById(clientId);
+
+            User user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            ApplyCreditLimit(user, client);
+
+            if (user.HasCreditLimit && user.CreditLimit < 500) return false;
+            
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+    }
+}
---
+++ LAB04/UnitTests/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using LegacyApp;
+
+namespace UnitTests
+{
+    internal class Program
+    {
+        static int TestCounter = 0;
+        static void Apply(bool Result, bool Truth)
+        {
+            if(Result == Truth)
+            {
+                Console.WriteLine("Test: " + TestCounter + " Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Test: " + TestCounter + " Failed!");
+            }
+            TestCounter++;
+        }
+
+        static void Main(string[] args)
+        {
+            UserService userService = new UserService();
+            Apply(userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1), true);
+            Apply(userService.AddUser("John", null, "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1), false);
+            Apply(userService.AddUser(null, "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1), false);
+            Apply(userService.AddUser(null, null, "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1), false);
+            Apply(userService.AddUser("John", "Doe", "johndoe@gmailcom", DateTime.Parse("1982-03-21"), 1), true);
+            Apply(userService.AddUser("John", "Doe", "johndoegmail.com", DateTime.Parse("1982-03-21"), 1), true);
+            Apply(userService.AddUser("John", "Doe", "johndoegmailcom", DateTime.Parse("1982-03-21"), 1), false);
+            Apply(userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("2007-03-21"), 1), false);
+        }
+    }
+}
