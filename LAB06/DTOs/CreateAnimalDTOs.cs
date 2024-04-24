e2de02a09354db2f10bf522ba52d537c37025d46
truew1n <48839784+truew1n@users.noreply.github.com>
2024-04-24 22:53:00 +0200

    LAB06

--- LAB06/DTOs/CreateAnimalDTOs.cs
+++ LAB06/DTOs/CreateAnimalDTOs.cs
@@ -5,7 +5,7 @@ namespace LAB06.DTOs
 
     public record CreateAnimalRequest(
         [Required][MaxLength(200)] string Name,
-        [Required][MaxLength(200)] string Description,
+        [MaxLength(200)] string Description,
         [Required][MaxLength(200)] string Category,
         [Required][MaxLength(200)] string Area
     );
--- LAB06/DTOs/UpdateAnimalsDTOs.cs
+++ LAB06/DTOs/UpdateAnimalsDTOs.cs
@@ -4,7 +4,7 @@ namespace LAB06.DTOs
 {
     public record UpdateAnimalRequest(
         [Required][MaxLength(200)] string Name,
-        [Required][MaxLength(200)] string Description,
+        [MaxLength(200)] string Description,
         [Required][MaxLength(200)] string Category,
         [Required][MaxLength(200)] string Area
     );
